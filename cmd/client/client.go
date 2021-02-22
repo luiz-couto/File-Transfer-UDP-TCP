a9e5c58a4511a121494ec2037622747a95c57920
luiz-couto <luiz.felipe.556@outlook.com>
2021-02-22 08:33:00 -0300

    start udp conn client side

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -6,18 +6,56 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/luiz-couto/File-Transfer-UDP-TCP/pkg/bytes"
 	"github.com/luiz-couto/File-Transfer-UDP-TCP/pkg/message"
 )
 
-func handleMsg(msg []byte) {
+// File DOC TODO
+type File struct {
+	fileSize int
+	fileName string
+	content  []byte
+}
+
+// SlidingWindow DOC TODO
+type SlidingWindow struct {
+	windowSize int
+	nextWindow []int
+}
+
+// Client DOC TODO
+type Client struct {
+	UDPconn   *net.UDPConn
+	TCPconn   net.Conn
+	SliWindow *SlidingWindow
+	file      *File
+}
+
+func (c *Client) startUDPConnection(port int) {
+	connectTo := strings.Split(c.TCPconn.RemoteAddr().String(), ":")[0]
+
+	addr, err := net.ResolveUDPAddr("udp", connectTo+":"+strconv.Itoa(port))
+	udpConn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	c.UDPconn = udpConn
+}
+
+func (c *Client) handleMsg(msg []byte) {
 	msgID := bytes.ReadByteBlockAsInt(0, 2, msg)
 	switch msgID {
 	case message.ConnectionType:
 		fmt.Println("Received CONNECTION")
 		port := bytes.ReadByteBlockAsInt(2, 6, msg)
+
 		fmt.Println("Porto UDP is " + strconv.Itoa(port))
+		c.startUDPConnection(port)
+
 	}
 }
 
@@ -46,7 +84,12 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		handleMsg(msg)
+
+		client := &Client{
+			TCPconn: conn,
+		}
+
+		client.handleMsg(msg)
 
 	}
 }
